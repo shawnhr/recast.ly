6d6cbb416dcebaa2c60f47f65918a579c3624161
shawnhr <shawnk0818@gmail.com>
2018-02-04 23:33:30 -0800

    Creates a dynamic video player component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {
+      props.videos.map(function(newVid) {
+        return <VideoListEntry video={newVid} />
+      })
+    }
   </div>
 );
 
